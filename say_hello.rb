ffe4030af353ee623af36106ed1acd2f68618184
Micah White <micahjwhite@gmail.com>
2020-04-08 19:27:53 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,4 +1,8 @@
+<<<<<<< HEAD
 def say_hello(name = "Ruby Programmer")
+=======
+def say_hello(name = "Ruby programmer")
+>>>>>>> 447c3b7305c9acdd6d42986de34dc33b0ded19da
   puts "Hello #{name}!"
 end
 
